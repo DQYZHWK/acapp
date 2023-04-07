6840d10b4b91b1786a15187c8ddcc3672b444d5a
DQYZHWK <dqyzhwk@qq.com>
2023-04-07 21:00:28 +0800

    4_7debug

--- game/urls/index.py
+++ game/urls/index.py
@@ -1,8 +1,13 @@
-from django.urls import path,include
+
+from django.urls import path, include
+from game.views.index import index
+
 
 urlpatterns = [
-    path =("menu/",include("game.urls.menu.index")),
-    path=("playground/",include("game.urls.playground.index")),
-    path==("setting/",include("game.urls.setting.index")),
-        
+    path("", index, name="index"),
+    path("menu/", include("game.urls.menu.index")),
+    path("playground/", include("game.urls.playground.index")),
+    path("settings/", include("game.urls.setting.index")),
 ]
+
+
